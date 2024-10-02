d2301ef78a3e0cd6fc59a6f4ec78453a5b6c64b6
razedulislam <blindtech2021@gmail.com>
2024-10-02 21:29:00 +0600

    cicd testing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from CI/CD Docker Container! I love this\n');
+  res.end('Hello from CI/CD Docker Container! I love my wife\n');
 });
 
 server.listen(port, hostname, () => {
