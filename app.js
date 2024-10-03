14326bb5e9f39c3c87f952da4d6cb87d73ed31ac
razedulislam <blindtech2021@gmail.com>
2024-10-03 08:13:56 +0600

    html file adds -for testing purposes adds some html code

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from CI/CD Docker Container! This is my first cicd pipeline\n');
+    const filePath = path.join(__dirname, 'index.html');
+    
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('Internal Server Error\n');
+        } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }
+    });
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
