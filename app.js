e0ea1435e3b92af0c117f671d81cf51fdeafe101
razedulislam <blindtech2021@gmail.com>
2024-10-03 08:02:06 +0600

    some text adds for test purpose -This is really awesome cicd pipeline that I enjoyed much

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from CI/CD Docker Container! I love my wife\n');
+  res.end('Hello from CI/CD Docker Container! This is my first cicd pipeline\n');
 });
 
 server.listen(port, hostname, () => {
